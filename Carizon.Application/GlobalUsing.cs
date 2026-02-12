27d55065838d0b2c82ee568351230b5cc410fb59
Ahmed-Abdulrahim <hemo089@outlook.com>
2026-02-12 16:12:54 +0200

    Implement generic Repository pattern with Specification support

--- Carizon.Application/GlobalUsing.cs
+++ Carizon.Application/GlobalUsing.cs
@@ -1,1 +1,3 @@
 ﻿global using Microsoft.Extensions.DependencyInjection;
+global using Carizon.Domain.Models;
+global using System.Linq.Expressions;
--- Carizon.Infrastructure/GlobalUsing.cs
+++ Carizon.Infrastructure/GlobalUsing.cs
@@ -6,5 +6,7 @@ global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 global using Carizon.Infrastructure.Presistence.DbContext;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.AspNetCore.Identity;
-
+global using Carizon.Application.Interfaces;
+global using System.Linq.Expressions;
 global using Microsoft.EntityFrameworkCore.Migrations;
+global using Carizon.Infrastructure.Specifications;
